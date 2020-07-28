40bde41ce5720b100ff36bde5fe5b0ed83b4b77e
marceloarb <59586505+marceloarb@users.noreply.github.com>
2020-07-28 13:57:27 -0500

    Inicial commit

---
+++ src/main/java/com/teksystems/TekcampExerciseSpring/TekcampExerciseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.teksystems.TekcampExerciseSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekcampExerciseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekcampExerciseSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teksystems/TekcampExerciseSpring/TekcampExerciseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teksystems.TekcampExerciseSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekcampExerciseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
